1afae6068541389b4b4a7221d4c19635959b4f13
Abhijit2505 <abhijittripathy99@gmail.com>
2020-12-06 14:22:13 +0530

    Added edualgo showcase page

---
+++ assets/js/showcase.js
@@ -0,0 +1,11 @@
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 1000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
